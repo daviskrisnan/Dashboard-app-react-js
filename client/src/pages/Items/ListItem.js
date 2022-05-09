befe8b6e270d5e5fd4c6fcac9fa627a52b053ac4
Davis Krisnan <daviskrisnan@gmail.com>
2022-05-09 18:35:14 +0700

    Change Item Table List to Card List

--- client/src/pages/Items/ListItem.js
+++ client/src/pages/Items/ListItem.js
@@ -23,9 +23,9 @@ const ListItem = () => {
 
   return (
     <>
-        <div className='row my-3 text-center'>
-            <div className='col-9 mx-auto'>
-                <div className='w-100'>
+        <div className='row g-2'>
+
+                <div className='my-3 text-center'>
                     <Link to="/add" className='btn btn-sm btn-primary my-2'>
                         <span className='me-2'>
                             <FaPlus />
@@ -33,49 +33,34 @@ const ListItem = () => {
                         Add Items
                     </Link>
                 </div>
-                <div className='w-100'>
-                    <table className='table table-bordered'>
-                        <thead>
-                            <tr>
-                                <th>No</th>
-                                <th>Nama</th>
-                                <th>Harga</th>
-                                <th>Kategori</th>
-                                <th>Brand</th>
-                                <th>User</th>
-                                <th>Aksi</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                items.length > 0 ?
-                                items.map(item => {
-                                    const { id, name, price, category, brand, user, image } = item;
-                                    return (
-                                        <tr key={id}>
-                                            <td>{id}</td>
-                                            <td>
-                                                <div className='w-10 mb-3 text-center' >
-                                                    <img src={image} className='img-flud rounded-circle'></img>
-                                                </div>
-                                                {name}
-                                            </td>
-                                            <td>Rp. {price}</td>
-                                            <td>{category}</td>
-                                            <td>{brand.name}</td>
-                                            <td>{user.name}</td>
-                                            <td>
-                                                <Link to={`/edit/${id}`} className='btn btn-sm btn-info'>Edit</Link>
-                                                <button onClick={ () => deleteHandler(+id) } className='btn btn-sm btn-danger'>Delete</button>
-                                            </td>
-                                        </tr>
-                                    )
-                                }) : <LoadingBar />
-                            }
-                        </tbody>
-                    </table>
-                </div>
-            </div>
+                {
+                    items.length > 0 ?
+                    items.map(item => {
+                        const { id, name, price, category, brand, user, image } = item;
+                        return (
+                            <div key={id} className='card col-sm-11 col-md-5 col-lg-3 mx-5'>
+                                <img className='card-img-top' src={image} alt='Card cap'></img>
+                                <div className='card-body'>
+                                    <h5 className='card-title text-center'>{name}</h5>
+                                    <p className='card-title'>Rp. {price}</p>
+                                    <p className='card-title'><strong>{category}</strong></p>
+                                    <p className='card-title'>{brand.name}</p>
+                                    <p className='card-title'>{user.name}</p>
+
+                                    <div className='row col-12 mx-auto'>
+                                        <div className='col-2 text-center'>
+                                            <Link to={`/edit/${id}`} className='btn btn-sm btn-info'>Edit</Link>
+                                        </div>
+                                        <div className='col-2 text-center'>
+                                            <button onClick={ () => deleteHandler(+id) } className='btn btn-sm btn-danger'>Delete</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    }) : <LoadingBar/>
+                }
+
         </div>
     </>
   )
